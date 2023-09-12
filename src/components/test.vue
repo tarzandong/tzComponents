60de37105788920d38a6649fbaa36c33a7491d44
dongtaizhong <dongtaizhong@csgis.cn>
2023-09-12 11:58:50 +0800

    vite 插件 生成wxx的css class

---
+++ src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w81 h35 lh20 fs16 bcwarning">test</div>
+  <div class="w188 h66 bcdark"></div>
+  <div class="w81"></div>
+  <div class="w20 h22 test1"></div>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style lang='scss' scoped>
+  .test1 {
+    background-color: colors(gray1);
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {whClassPlugin} from './src/vite-plugin/whClass.js'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [whClassPlugin(), vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
