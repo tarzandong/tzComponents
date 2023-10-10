5db19c4399ddd090a92c06a38064e14b5861b92e
dongtaizhong <dongtaizhong@csgis.cn>
2023-10-10 08:46:29 +0800

    autoclass 1.1.3

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w105 h34 lh30 fs16 bcwarning bdr10 ml20 pl20 op130">test</div>
+  <div class="w105 h88 lh30 fs16 bcwarning bdr10 ml20 pl20 op130">test</div>
   <div class="vw30 h180 bcdark mt45 bdr8"></div>
   <div class="pw20 h22 test1 m20"></div>
 </template>
